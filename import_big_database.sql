71e6366b1d3e3091791b436cdca8b584c4a6d7bb
David Duong <topcoder90@gmail.com>
2021-08-10 16:27:14 +0700

    Update import_big_database.sql

--- import_big_database.sql
+++ import_big_database.sql
@@ -1,7 +1,9 @@
 Import big database
 
+export
 mysqldump -u user -p namedatabase | gzip > namedatabase.sql.gz
 password_database
 
+import
 gunzip < namedatabase.sql.gz | mysql -u user -p namedatabase
 password_database
